d764e612d0d87e09b7fe0ef45e22c2c8befa1b16
ItaloSSilva <italo.s.silva@rede.ulbra.br>
2022-06-14 23:21:29 -0300

    condicional de categoria

--- index.php
+++ index.php
@@ -2,7 +2,19 @@
 
 $listaCategorias = [];
 $listaCategorias[0] = 'infantil';
-$listaCategorias[1] = 'adolecente';
+$listaCategorias[1] = 'adolescente';
 $listaCategorias[2] = 'adulto';
 
-print_r($listaCategorias);
+$nome = 'Pedro';
+$idade = 17;
+
+
+if($idade >= 6 && $idade <= 12){
+    echo($listaCategorias[0]);
+} elseif($idade > 12 && $idade <=18) {
+    echo($listaCategorias[1]);
+} elseif($idade > 18){
+    echo($listaCategorias[2]);
+} else {
+    echo('Não possui idade adequada.');
+}
